d413f3d623f011f033c2d07081ec23d1483faf1b
James Coholan <52676083+jamesccoholan@users.noreply.github.com>
2022-07-15 22:53:53 -0400

    commit

---
+++ src/Components/Header.js
@@ -0,0 +1,18 @@
+import "./Header.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header2">
+        <h3>Hello</h3>
+      </header>
+
+      <div className="App-header2">
+        <img src="/assets/DAC_.png" alt="Girl in a jacket" display="flex" />
+        <img src="/assets/DAC_.png" alt="Girl in a jacket" display="flex" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
