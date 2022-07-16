015e5099d03a55d0236403f836e7aec7b75583aa
James Coholan <52676083+jamesccoholan@users.noreply.github.com>
2022-07-16 08:53:59 -0400

    commit

---
+++ src/Components/CastPages.js
@@ -0,0 +1,51 @@
+import "./CastPages.css";
+
+function CastPages() {
+  return (
+    <div className="CastPages-Main">
+      <div className="CastPages">
+        <div>
+          <img
+            src="/assets/Website 2/01 Landing Page (Home)/LandingPage_BG.jpg"
+            alt="Hero Image"
+            display="flex"
+            height="20%"
+            width="20%"
+          />
+          <img />
+          <img
+            src="/assets/Website 2/01 Landing Page (Home)/LandingPage_BG.jpg"
+            alt="Hero Image"
+            display="flex"
+            height="20%"
+            width="20%"
+          />
+          <img />
+          <img
+            src="/assets/Website 2/01 Landing Page (Home)/LandingPage_BG.jpg"
+            alt="Hero Image"
+            display="flex"
+            height="20%"
+            width="20%"
+          />
+        </div>
+        <img />
+        {/* <div class="bottom-left">
+          This animated parody of a House Reality show features cast members
+          from five different NFT communities who are transported to LA to
+          experience life in the real world. And, just like a reality TV show,
+          our cast is ready for primetime! Fights? Parties? Bad Advice? Let’s
+          just say, they’ll share a house, make some jokes, make some mistakes,
+          and maybe even make out! Join our community to influence cast
+          decisions, create episodes, and control the fate of Season 2!
+        </div> */}
+        {/* <div class="top-left">Top Left</div>
+        <div class="top-right">Top Right</div>
+        <div class="bottom-right">Bottom Right</div>
+        <div class="centered">Centered</div> */}
+      </div>
+    </div>
+  );
+}
+
+export default CastPages;
