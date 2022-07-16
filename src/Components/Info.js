ebb27f03d0b5ba6a5aa8c84392cdc3090b68d08d
James Coholan <52676083+jamesccoholan@users.noreply.github.com>
2022-07-16 17:33:07 -0400

    commit

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -5,9 +5,11 @@ function Info() {
     <div className="CastPages-Main">
       <div className="CastPages">
         <div className="Roadmap">
+          <section id="Info-section" />
           <img
             src="/assets/Website 2/06 Value Prop/info.png"
             className="Roadmap-Main"
+            alt="alt"
           ></img>
         </div>
       </div>
