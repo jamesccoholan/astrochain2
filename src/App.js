17c587b866d9c62d9af7552a4162a0df4d8de185
James Coholan <52676083+jamesccoholan@users.noreply.github.com>
2022-07-15 23:33:49 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import Header from "./Components/Header";
 import NavBar from "./Components/NavBar";
+import {
+  ScrollingProvider,
+  useScrollSection,
+  Section,
+} from "react-scroll-section";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Header />
-      <a href="#news">News</a>
+      <a component={Section} id="collection" href="#news">
+        Here
+      </a>
       <header className="App-header">
         <h3>Hello</h3>
       </header>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,39 +1,94 @@
 import "./NavBar.css";
-import { Button } from "@mui/material";
-// import { AppBar, Grid, Button, Toolbar, Container } from "@mui/material";
+import { Button, AppBar, Toolbar, Grid, Container } from "@mui/material";
 
 import FacebookIcon from "@mui/icons-material/Facebook";
 import PinterestIcon from "@mui/icons-material/Pinterest";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 
+import { useScrollSection } from "react-scroll-section";
+
 function NavBar() {
+  const collectionSection = useScrollSection("collection");
+  const vbCaresSection = useScrollSection("vb-cares");
+  const roadmapSection = useScrollSection("roadmap");
+  const faqSection = useScrollSection("faq");
+
   return (
     <div class="topnav" id="myTopnav">
-      <a href="#home" class="active">
-        Home
-      </a>
-      <a href="#news">News</a>
-      <a href="#contact">Contact</a>
-      <a href="#about">About</a>
-      <a href="javascript:void(0);" class="icon" onclick="myFunction()">
-        <i class="fa fa-bars"></i>
-      </a>
-      <Button href="https://twitter.com/verabradley">
-        <TwitterIcon sx={{ mr: 1, color: "#000000" }} />
-      </Button>
-      <Button href="https://www.facebook.com/verabradley">
-        <FacebookIcon
-          href="https://discord.com"
-          sx={{ mr: 1, color: "#000000" }}
-        />
-      </Button>
-      <Button href="https://www.pinterest.com/verabradley/">
-        <PinterestIcon sx={{ mr: 1, color: "#000000" }} />
-      </Button>
-      <Button href="https://www.instagram.com/verabradley/">
-        <InstagramIcon sx={{ mr: 1, color: "#000000" }} />
-      </Button>
+      <AppBar position="static" elevation={0} sx={{ backgroundColor: "blue" }}>
+        <Toolbar disableGutters={true} sx={{ flexWrap: "wrap" }}>
+          <Grid container sx={{ display: "flex", flex: "0 0 100%" }}>
+            <Grid
+              item
+              sx={{ backgroundColor: "#f3f3f3", color: "#333" }}
+              xs={12}
+              display={"flex"}
+              justifyContent={"center"}
+              py={1}
+            >
+              <Button
+                variant={"text"}
+                size={"large"}
+                color={"inherit"}
+                onClick={collectionSection.onClick}
+              >
+                Collection
+              </Button>
+              <Button
+                variant={"text"}
+                size={"large"}
+                color={"inherit"}
+                onClick={vbCaresSection.onClick}
+              >
+                VB Cares
+              </Button>
+              <Button
+                variant={"text"}
+                size={"large"}
+                color={"inherit"}
+                onClick={roadmapSection.onClick}
+              >
+                Roadmap
+              </Button>
+              <Button
+                variant={"text"}
+                size={"large"}
+                color={"inherit"}
+                onClick={faqSection.onClick}
+              >
+                FAQ
+              </Button>
+            </Grid>
+          </Grid>
+          {/* <a href="#home" class="active">
+            Home
+          </a>
+          <a href="#news">News</a>
+          <a href="#contact">Contact</a>
+          <a href="#about">About</a>
+          <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+          </a> */}
+          <div>
+            <Button href="https://twitter.com/verabradley">
+              <TwitterIcon sx={{ mr: 1, color: "#000000" }} />
+            </Button>
+            <Button href="https://www.facebook.com/verabradley">
+              <FacebookIcon
+                href="https://discord.com"
+                sx={{ mr: 1, color: "#000000" }}
+              />
+            </Button>
+            <Button href="https://www.pinterest.com/verabradley/">
+              <PinterestIcon sx={{ mr: 1, color: "#000000" }} />
+            </Button>
+            <Button href="https://www.instagram.com/verabradley/">
+              <InstagramIcon sx={{ mr: 1, color: "#000000" }} />
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
