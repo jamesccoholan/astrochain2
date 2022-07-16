df301088f99fdaba8f65a722106e8c52b3fae180
James Coholan <52676083+jamesccoholan@users.noreply.github.com>
2022-07-16 17:36:30 -0400

    commit

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -66,7 +66,7 @@ export default function NavBar() {
                 size={"large"}
                 color={"inherit"}
                 onClick={faqSection.onClick}
-                href="#about-section"
+                href="#Info-section"
               >
                 Value Prop
               </Button>
