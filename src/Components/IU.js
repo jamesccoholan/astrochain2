d6d424499b4de38decbe5363696ae29e4c28b156
James Coholan <52676083+jamesccoholan@users.noreply.github.com>
2022-07-16 16:12:03 -0400

    commit

--- src/Components/IU.js
+++ src/Components/IU.js
@@ -4,84 +4,55 @@ function IU() {
   return (
     <div className="CastPages-Main">
       <div className="CastPages">
+        <h7>INVISIBLE UNIVERSE</h7>
         <div className="videos">
-          <video
-            autoPlay
-            loop
-            muted
-            playsinline
-            webkit-playsinline
-            width="220"
-            height="220"
+          <img
+            src="/assets/Website 2/07 Invisible _ Core Team/01_CoreTeam_Dan.png"
+            width="180"
+            height="180"
             className="videos"
-          >
-            <source
-              src={"assets/mp4/01_Pete_2dTo3d (Stitched Clip).mp4"}
-              type="video/mp4"
-            />
-          </video>
-          <video
-            autoPlay
-            loop
-            muted
-            playsinline
-            webkit-playsinline
-            width="220"
-            height="220"
+          ></img>
+          <img
+            src="/assets/Website 2/07 Invisible _ Core Team/02_CoreTeam_Ben.png"
+            width="180"
+            height="180"
             className="videos"
-          >
-            <source
-              src={"assets/mp4/02_Monty_2dTo3d (Stitched Clip).mp4"}
-              type="video/mp4"
-            />
-          </video>
-          <video
-            autoPlay
-            loop
-            muted
-            playsinline
-            webkit-playsinline
-            width="220"
-            height="220"
+          ></img>
+          <img
+            src="/assets/Website 2/07 Invisible _ Core Team/03_CoreTeam_Van.png"
+            width="180"
+            height="180"
             className="videos"
-          >
-            <source src={"assets/mp4/SAMS.mp4"} type="video/mp4" />
-          </video>
-          <video
-            autoPlay
-            loop
-            muted
-            playsinline
-            webkit-playsinline
-            width="220"
-            height="220"
+          ></img>
+          <img
+            src="/assets/Website 2/07 Invisible _ Core Team/04_CoreTeam_Yohann.png"
+            width="180"
+            height="180"
             className="videos"
-          >
-            <source
-              src={"assets/mp4/04_Journey_2dTo3d (Stitched Clip).mp4"}
-              type="video/mp4"
-            />
-          </video>
-          <video
-            autoPlay
-            loop
-            muted
-            playsinline
-            webkit-playsinline
-            width="220"
-            height="220"
+          ></img>
+          <img
+            src="/assets/Website 2/07 Invisible _ Core Team/05_CoreTeam_Fubs.png"
+            width="180"
+            height="180"
             className="videos"
-          >
-            <source
-              src={"assets/mp4/05_Claudine_2dTo3d (Stitched Clip).mp4"}
-              type="video/mp4"
-            />
-          </video>
+          ></img>
+          <img
+            src="/assets/Website 2/07 Invisible _ Core Team/06_CoreTeam_Henrique.png"
+            width="180"
+            height="180"
+            className="videos"
+          ></img>
+          <img
+            src="/assets/Website 2/07 Invisible _ Core Team/07_CoreTeam_Bruno.png"
+            width="180"
+            height="180"
+            className="videos"
+          ></img>
           <div />
           <div className="description">
             <h6>
               {
-                "What started off as a PFP in 2 dimensions has evolved into a fully realized 3D character with an oversized personality. Take some time to read through the cast bios, admire the artists' process and learn how we created this first of its kind series."
+                "Invisible Universe is an internet-first animation studio that develops original character IP on social media and web3 platforms in partnership with some of the world's biggest celebrities. Our mission is to create the next household name animated franchises faster and more economical than traditional models. We believe the next generation of animated franchises will be born on the internet. The company is backed by some of the best media and technology investors in Hollywood and Silicon Valley, including Alexis Ohanian's Seven Seven Six, Initialized Capital, The Chernin Group, Serena Williams, Will Smith's Dreamers VC, CASSIUS Family, Spencer Rascoff's 75 & Sunny VC, and many more."
               }
             </h6>
           </div>
--- src/Components/Rarity.js
+++ src/Components/Rarity.js
@@ -4,87 +4,11 @@ function EpisodeGuide() {
   return (
     <div className="CastPages-Main">
       <div className="CastPages">
-        <div className="videos">
-          <video
-            autoPlay
-            loop
-            muted
-            playsinline
-            webkit-playsinline
-            width="220"
-            height="220"
-            className="videos"
-          >
-            <source
-              src={"assets/mp4/01_Pete_2dTo3d (Stitched Clip).mp4"}
-              type="video/mp4"
-            />
-          </video>
-          <video
-            autoPlay
-            loop
-            muted
-            playsinline
-            webkit-playsinline
-            width="220"
-            height="220"
-            className="videos"
-          >
-            <source
-              src={"assets/mp4/02_Monty_2dTo3d (Stitched Clip).mp4"}
-              type="video/mp4"
-            />
-          </video>
-          <video
-            autoPlay
-            loop
-            muted
-            playsinline
-            webkit-playsinline
-            width="220"
-            height="220"
-            className="videos"
-          >
-            <source src={"assets/mp4/SAMS.mp4"} type="video/mp4" />
-          </video>
-          <video
-            autoPlay
-            loop
-            muted
-            playsinline
-            webkit-playsinline
-            width="220"
-            height="220"
-            className="videos"
-          >
-            <source
-              src={"assets/mp4/04_Journey_2dTo3d (Stitched Clip).mp4"}
-              type="video/mp4"
-            />
-          </video>
-          <video
-            autoPlay
-            loop
-            muted
-            playsinline
-            webkit-playsinline
-            width="220"
-            height="220"
-            className="videos"
-          >
-            <source
-              src={"assets/mp4/05_Claudine_2dTo3d (Stitched Clip).mp4"}
-              type="video/mp4"
-            />
-          </video>
-          <div />
-          <div className="description">
-            <h6>
-              {
-                "What started off as a PFP in 2 dimensions has evolved into a fully realized 3D character with an oversized personality. Take some time to read through the cast bios, admire the artists' process and learn how we created this first of its kind series."
-              }
-            </h6>
-          </div>
+        <div className="Roadmap">
+          <img
+            src="/assets/Website 2/05 Rarity/RarityTemp.png"
+            className="Roadmap-Main"
+          ></img>
         </div>
       </div>
     </div>
