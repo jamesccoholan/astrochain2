2949921457120ddc696a6a4da3b8c7ed8fd4ef83
James Coholan <52676083+jamesccoholan@users.noreply.github.com>
2022-07-16 17:47:26 -0400

    commit

--- src/Components/Roadmap.js
+++ src/Components/Roadmap.js
@@ -3,13 +3,24 @@ import "./Roadmap.css";
 function Roadmap() {
   return (
     <div>
-      <div className="Roadmap">
+      <div className="Roadmap-Main2">
         <section id="roadmap-section" />
-        <img
+        {/* <img
           src="/assets/Website 2/03 Road Map/RM.png"
           className="Roadmap-Main"
           alt="alt"
-        ></img>
+        ></img> */}
+        <video
+          autoPlay
+          loop
+          muted
+          playsinline
+          webkit-playsinline
+          width="100%"
+          height="100%"
+        >
+          <source src={"/assets/RoadMap_BG_v03.mp4"} type="video/mp4" />
+        </video>
       </div>
     </div>
   );
