461a61209fd0e9309ea4a004d12a86dfea94f3f3
Stephen Dorn <stephendorn@stephens-mbp-2.lan>
2022-07-17 19:54:36 -0700

    working on header and first section

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,34 +2,26 @@ import "./Header.css";
 
 function Header() {
   return (
-    <div className="App-header">
-      <div className="App-header2">
+    <div className="Header">
         <a href="#about-section" />
         <img
-          src="/assets/Website 2/01 Landing Page (Home)/3.jpg"
+          src="/assets/Website 2/01 Landing Page (Home)/LandingPage_BG.jpg"
           alt="Hero"
-          position="absolute"
-          height="100%"
-          width="100%"
-          left="50%"
-          top="50%"
-          max-width="500px"
-          objectFit="cover"
+          class="landing-background-img"
         />
-        <div class="bottom-left">
+        <img
+          src="/assets/Website 2/01 Landing Page (Home)/LandingPage_Logo.png"
+          class="landing-logo"
+        />
+        <div class="landing-bottom-text">
           This animated parody of a House Reality show features cast members
           from five different NFT communities who are transported to LA to
           experience life in the real world. And, just like a reality TV show,
-          our cast is ready for primetime! Fights? Parties? Bad Advice? Let’s
+          our cast is ready for primetime! <br />Fights? Parties? Bad Advice? Let’s
           just say, they’ll share a house, make some jokes, make some mistakes,
           and maybe even make out! Join our community to influence cast
           decisions, create episodes, and control the fate of Season 2!
         </div>
-        {/* <div class="top-left">Top Left</div>
-        <div class="top-right">Top Right</div>
-        <div class="bottom-right">Bottom Right</div>
-        <div class="centered">Centered</div> */}
-      </div>
     </div>
   );
 }
