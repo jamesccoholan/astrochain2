77dc30359cdd90ffabf78ab80820e17cf57a51ec
James Coholan <52676083+jamesccoholan@users.noreply.github.com>
2022-07-18 08:49:11 -0400

    commit

--- src/Components/Info.js
+++ src/Components/Info.js
@@ -2,9 +2,51 @@ import "./Info.css";
 
 function Info() {
   return (
-    <div className="CastPages-Main">
+    <div className="info-main">
       <section id="Info-section" />
-      <img src="/assets/icons2.png" className="Roadmap-Main" alt="alt"></img>
+      <div className="videos">
+        <img
+          src="/assets/logos/BAC-SVG.webp"
+          width="180"
+          height="180"
+          className="videos"
+          alt="alt"
+        ></img>
+        <img
+          src="/assets/logos/logo.png"
+          width="180"
+          height="180"
+          className="videos"
+          alt="alt"
+        ></img>
+        <img
+          src="/assets/logos/Logo_709x.webp"
+          width="180"
+          height="180"
+          className="videos"
+          alt="alt"
+        ></img>
+        <img
+          src="/assets/logos/doodless.png"
+          width="180"
+          height="180"
+          className="videos"
+          alt="alt"
+        ></img>
+        <img
+          src="/assets/logos/Robotoss.png"
+          width="180"
+          height="180"
+          className="videos"
+          alt="alt"
+        ></img>
+        <div />
+        <div className="description-cast-pages">
+          {
+            "What started off as a PFP in 2 dimensions has evolved into a fully realized 3D character with an oversized personality. Take some time to read through the cast bios, admire the artists' process and learn how we created this first of its kind series."
+          }
+        </div>
+      </div>
     </div>
   );
 }
--- src/Components/Rarity.js
+++ src/Components/Rarity.js
@@ -5,42 +5,50 @@ function Rarity() {
   return (
     <div className="rarity-section">
       <h2 className="rarity-title">RARITY</h2>
-      <Stack direction={{xs:'column', sm:'column', md: 'row', lg:'row'}} spacing={{xs:0, sm:2, md:2, lg:4}} alignItems="top-text" width="90%" height="auto" object-fit="cover" style={{ justifyContent: 'center', padding: '0 0 40px 0' }}>
-      <div>
-        <div className="rarity-description">
-          Our Producer Passes are designed to become your Twitter banner. We chose
-          this underutilized stvle to accentuate the fact that while our
-          characters may originate in different communities, thev only exist
-          together here on The R3eal Metaverse.
+      <Stack
+        direction={{ xs: "column", sm: "column", md: "row", lg: "row" }}
+        spacing={{ xs: 0, sm: 2, md: 2, lg: 4 }}
+        alignItems="top-text"
+        width="90%"
+        height="auto"
+        object-fit="cover"
+        style={{ justifyContent: "center", padding: "0 0 40px 0" }}
+      >
+        <div>
+          <div className="rarity-description">
+            Our Producer Passes are designed to become your Twitter banner. We
+            chose this underutilized stvle to accentuate the fact that while our
+            characters may originate in different communities, thev only exist
+            together here on The R3eal Metaverse.
+          </div>
+          <div className="rarity-description">
+            Over 100 different traits algorithmi- cally combine iconic locations
+            in Los Angeles with award winning animation to generate over 4
+            million combinations. The Mint will randomly produce XXXX unique
+            Producer Passes.
+          </div>
+          <div className="rarity-description">
+            {" "}
+            In addition, 20 Ultra Rare Passes will be created. If you're lucky
+            enough to mint one of these rare NFT's you will automatically
+            receive 100 bonus points to be included on the Leader-board for a
+            chance to win amazing opportunities.
+          </div>
         </div>
-        <div className="rarity-description">
-          Over 100 different traits algorithmi- cally combine iconic locations in
-          Los Angeles with award winning animation to generate over 4 million
-          combinations. The Mint will randomly produce XXXX unique Producer
-          Passes.
+        <div className="rarity-pics">
+          <img
+            style={{ padding: "20px" }}
+            src="/assets/1.png"
+            className="Roadmap-Main"
+            alt="alt"
+          ></img>
+          <img
+            style={{ padding: "20px" }}
+            src="/assets/2.png"
+            className="Roadmap-Main"
+            alt="alt"
+          ></img>
         </div>
-        <div className="rarity-description">
-          {" "}
-          In addition, 20 Ultra Rare Passes will be created. If you're lucky
-          enough to mint one of these rare NFT's you will automatically receive
-          100 bonus points to be included on the Leader-board for a chance to win
-          amazing opportunities.
-        </div>
-      </div>
-      <div className="rarity-pics">
-        <img
-          style={{ padding: "20px" }}
-          src="/assets/1.png"
-          className="Roadmap-Main"
-          alt="alt"
-        ></img>
-        <img
-          style={{ padding: "20px" }}
-          src="/assets/2.png"
-          className="Roadmap-Main"
-          alt="alt"
-        ></img>
-      </div>
       </Stack>
     </div>
   );
