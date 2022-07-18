a85fd1781f0c75482bbe82bb1b95b42238b50ebb
James Coholan <52676083+jamesccoholan@users.noreply.github.com>
2022-07-17 23:09:54 -0400

    commit

--- src/Components/Rarity.js
+++ src/Components/Rarity.js
@@ -8,13 +8,7 @@ function Rarity() {
         Our Producer Passes are designed to become your Twitter banner. We chose
         this underutilized stvle to accentuate the fact that while our
         characters may originate in different communities, thev only exist
-        together here on The R3eal Metaverse. Over 100 different traits
-        algorithmi- cally combine iconic locations in Los Angeles with award
-        winning animation to generate over 4 million combinations. The Mint will
-        randomly produce XXXX unique Producer Passes. In addition, 20 Ultra Rare
-        Passes will be created. If you're lucky enough to mint one of these rare
-        NFT's you will automatically receive 100 bonus points to be included on
-        the Leader-board for a chance to win amazing opportunities.
+        together here on The R3eal Metaverse.
       </div>
       <div className="rarity-description">
         Over 100 different traits algorithmi- cally combine iconic locations in
@@ -23,13 +17,11 @@ function Rarity() {
         Passes.
       </div>
       <div className="rarity-description">
-        Over 100 different traits algorithmi- cally combine iconic locations in
-        Los Angeles with award winning animation to generate over 4 million
-        combinations. The Mint will randomly produce XXXX unique Producer
-        Passes. In addition, 20 Ultra Rare Passes will be created. If you're
-        lucky enough to mint one of these rare NFT's you will automatically
-        receive 100 bonus points to be included on the Leader-board for a chance
-        to win amazing opportunities.
+        {" "}
+        In addition, 20 Ultra Rare Passes will be created. If you're lucky
+        enough to mint one of these rare NFT's you will automatically receive
+        100 bonus points to be included on the Leader-board for a chance to win
+        amazing opportunities.
       </div>
       <div className="rarity-pics">
         <img src="/assets/1.png" className="Roadmap-Main" alt="alt"></img>
