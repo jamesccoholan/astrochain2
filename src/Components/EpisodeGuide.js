01bec78fce283ccb443d2b511b008b4942ac7c5b
Stephen Dorn <stephendorn@stephens-mbp-2.lan>
2022-07-18 14:31:07 -0700

    episode guide section

--- src/Components/EpisodeGuide.js
+++ src/Components/EpisodeGuide.js
@@ -2,9 +2,16 @@ import "./EpisodeGuide.css";
 
 function EpisodeGuide() {
   return (
-    <div className="EpisodeGuide-Main">
-        <section id="episodeGuide-section" />
-        <div></div>
+    <div className="episode-guide">
+        <section id="episode-guide-section" />
+        <div>
+          <text class="episode-guide-title">EPISODE GUIDE</text>
+          <text class="episode-guide-description">
+            {
+              "This series is where the Metaverse and Real World converge! Click on the highlighted link to watch the current episode of this one-of-a-kind series or click on any previous link to watch earlier episodes and catch up. New shows are released twice weekly and can be viewed here or on any social media platform."
+            }
+          </text>
+        </div>
     </div>
   );
 }
