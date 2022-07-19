037ec86776f50f7bb8691238bde58f211151538f
Stephen Dorn <stephendorn@stephens-mbp-2.lan>
2022-07-18 22:33:00 -0700

    header updates

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -11,13 +11,11 @@ function Header() {
           class="landing-logo"
         />
         <div class="landing-bottom-text">
-          This animated parody of a House Reality show features cast members
-          from five different NFT communities who are transported to LA to
-          experience life in the real world. And, just like a reality TV show,
-          our cast is ready for primetime! <br />Fights? Parties? Bad Advice? Let’s
-          just say, they’ll share a house, make some jokes, make some mistakes,
-          and maybe even make out! Join our community to influence cast
-          decisions, create episodes, and control the fate of Season 2!
+        What happens when you put a Bored Ape, a Cool Cat, a World of Women, a Roboto {"&"} a Doodle on a reality show together? We have no idea either,
+         but we{"’"}re betting they make some jokes, make some mistakes, and maybe even make out?!<br />
+         The Real Metaverse (TRM) is an animated parody where Big Brother meets The Real World. 
+         The cast, inspired and approved by the original creators from each community, are transported from the metaverse to LA to experience life on our side for the very first time!<br />
+         The only thing more crazy than the boom of NFTs is what happens when their worlds collide. Join us and become a Producer to create episodes, control the fate of Season 2 and possibly get your character animated!
         </div>
     </div>
   );
