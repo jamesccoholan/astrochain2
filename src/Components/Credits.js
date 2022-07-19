4ef4c0cb641814390e5394adb3f94046ec76e298
Stephen Dorn <stephendorn@stephens-mbp-2.lan>
2022-07-18 17:04:10 -0700

    insert credits

--- src/Components/Credits.js
+++ src/Components/Credits.js
@@ -5,48 +5,16 @@ function Credits() {
   return (
     <Stack direction={{xs:'column', sm:'column', md: 'row', lg:'row'}} spacing={{xs:0, sm:2, md:4, lg:10}} alignItems="center" width="100%" height="auto" object-fit="cover" overflow="auto" style={{ justifyContent: 'center', padding: '20px 0 40px 0' }}>
       <div class="credits-text">
-        If you're an Original Community NFT holder.. NFT Holders who are
-        also holders from an Original Community can submit to have their
-        character animated for a Season 1 cameo! One form each community will
-        win based on engagement. *Need to be able to verify they hold an NFT
-        from one of the communities. If you're an Original Commu- nity NFT
-        holder. NFT Holders who are also holders from an Original Community
-        can submit to have their character animated for a Season 1 cameo! One
-        form each community will win based on engagement. *Need to be able to
-        verify they hold an NFT from one of the communities.
+        Insert credits here.
       </div>
       <div class="credits-text">
-        If you're an Original Community NFT holder.. NFT Holders who are
-        also holders from an Original Community can submit to have their
-        character animated for a Season 1 cameo! One form each community will
-        win based on engagement. *Need to be able to verify they hold an NFT
-        from one of the communities. If you're an Original Commu- nity NFT
-        holder. NFT Holders who are also holders from an Original Community
-        can submit to have their character animated for a Season 1 cameo! One
-        form each community will win based on engagement. *Need to be able to
-        verify they hold an NFT from one of the communities.
+      Insert credits here.
       </div>
       <div class="credits-text">
-        If you're an Original Community NFT holder.. NFT Holders who are
-        also holders from an Original Community can submit to have their
-        character animated for a Season 1 cameo! One form each community will
-        win based on engagement. *Need to be able to verify they hold an NFT
-        from one of the communities. If you're an Original Commu- nity NFT
-        holder. NFT Holders who are also holders from an Original Community
-        can submit to have their character animated for a Season 1 cameo! One
-        form each community will win based on engagement. *Need to be able to
-        verify they hold an NFT from one of the communities.
+        Insert credits here.
       </div>
       <div class="credits-text">
-        If you're an Original Community NFT holder.. NFT Holders who are
-        also holders from an Original Community can submit to have their
-        character animated for a Season 1 cameo! One form each community will
-        win based on engagement. *Need to be able to verify they hold an NFT
-        from one of the communities. If you're an Original Commu- nity NFT
-        holder. NFT Holders who are also holders from an Original Community
-        can submit to have their character animated for a Season 1 cameo! One
-        form each community will win based on engagement. *Need to be able to
-        verify they hold an NFT from one of the communities.
+      Insert credits here.
       </div>
     </Stack>
   );
