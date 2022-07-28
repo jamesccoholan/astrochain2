fe121f3a7b6bd484b454d5619c502deba42db0b7
Stephen Dorn <stephendorn@Stephens-MacBook-Pro-2.local>
2022-07-28 13:55:21 -0700

    new episode guide rezising initial

--- src/Components/Episodes.js
+++ src/Components/Episodes.js
@@ -2,17 +2,18 @@ import "./Episodes.css";
 
 function Episodes() {
   return (
-    <div className="episode-guide">
-        <section id="episode-guide-section" />
-        <img src="/assets/Website 2/04 Episode Guide/Spot_Light_01_v02.png" class="spotlight-img" />
-        <div>
-          <text class="episode-guide-title">EPISODE GUIDE</text>
-          <text class="episode-guide-description">
-            {
-              "This is where the Metaverse and Real World converge! New episodes are released twice weekly. Click on the link to watch the current episode or click on any previous link to watch earlier episodes."
-            }
-          </text>
-        </div>
+    <div class="episode-guide">
+      <img class="background-img" src="https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/EpisodesGuide_BG_v03_UlwcvzPCW.jpg" />
+      <section id="episode-guide-section" />
+      <img src="/assets/Website 2/04 Episode Guide/Spot_Light_01_v02.png" class="spotlight-img" />
+      <div>
+        <text class="episode-guide-title">EPISODE GUIDE</text>
+        <text class="episode-guide-description">
+          {
+            "This is where the Metaverse and Real World converge! New episodes are released twice weekly. Click on the link to watch the current episode or click on any previous link to watch earlier episodes."
+          }
+        </text>
+      </div>
     </div>
   );
 }
