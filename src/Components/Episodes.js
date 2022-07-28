d53facdcdeea43103ecd7c77f5a21b9a4c4c9f8a
Stephen Dorn <stephendorn@Stephens-MacBook-Pro-2.local>
2022-07-28 14:18:37 -0700

    redo the NFT Rarity section to be inline

--- src/Components/Episodes.js
+++ src/Components/Episodes.js
@@ -5,7 +5,7 @@ function Episodes() {
     <div class="episode-guide">
       <img class="background-img" src="https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/EpisodesGuide_BG_v03_UlwcvzPCW.jpg" />
       <section id="episode-guide-section" />
-      <img src="/assets/Website 2/04 Episode Guide/Spot_Light_01_v02.png" class="spotlight-img" />
+      <img  class="spotlight-1-img" src="https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/Spot_Light_01.png" />
       <div>
         <text class="episode-guide-title">EPISODE GUIDE</text>
         <text class="episode-guide-description">
--- src/Components/Rarity.js
+++ src/Components/Rarity.js
@@ -5,37 +5,37 @@ function Rarity() {
   return (
     <div class="rarity-section">
       <section id="rarity-section" />
-      <Stack
-        direction={{ xs: "column", sm: "column", md: "column", lg: "row" }}
-        spacing={{ xs: 0, sm: 0, md: 2, lg: 4 }}
-        alignItems="top-text"
-        height="auto"
-        object-fit="cover"
-        style={{ justifyContent: "center", padding: "0 0 40px 0" }}
-      >
-        <div>
-        <h2 class="rarity-title">NFT RARITY</h2>
-          <div class="rarity-description">
-            Our NFT is a Producer Pass designed to become your Twitter banner. We chose this style to accentuate the fact that while our characters may originate in different communities, they only exist together here on The R3eal Metaverse. 
-            Our traits algorithmically combine iconic locations in Los Angeles with award winning animation to generate over a million combinations. The Mint will randomly produce 7000 unique Producer Passes. 
+      <div class="rarity-container">
+        <Stack
+          direction={{ xs: "column", sm: "column", md: "column", lg: "row" }}
+          spacing={{ xs: 0, sm: 0, md: 2, lg: 4 }}
+          alignItems="top-text"
+          height="auto"
+          object-fit="cover"
+          style={{ justifyContent: "center", padding: "0 0 40px 0" }}
+        >
+          <div class="rarity-text-container">
+            <h2 class="rarity-title">NFT RARITY</h2>
+            <div class="rarity-description">
+              Our NFT is a Producer Pass designed to become your Twitter banner. We chose this style to accentuate the fact that while our characters may originate in different communities, they only exist together here on The R3eal Metaverse. 
+              Our traits algorithmically combine iconic locations in Los Angeles with award winning animation to generate over a million combinations. The Mint will randomly produce 7000 unique Producer Passes.<br/><br/>
+              In addition, 20 Ultra Rare Passes will be created. If you{"’"}re lucky enough to mint one of these rare NFT{"’"}s you will automatically receive 50 bonus points to be included on the Leaderboard.
+            </div>
           </div>
-          <div class="rarity-description">
-            In addition, 20 Ultra Rare Passes will be created. If you{"’"}re lucky enough to mint one of these rare NFT{"’"}s you will automatically receive 50 bonus points to be included on the Leaderboard.
+          <div class="rarity-pics">
+            <img
+              style={{ paddingTop: "20px" }}
+              src="https://ik.imagekit.io/5ierklngtbt/IU/05_Rarity/NFT_Common_gBs2-01D1.jpg"
+              alt="alt"
+            ></img>
+            <img
+              style={{ paddingTop: "20px" }}
+              src="https://ik.imagekit.io/5ierklngtbt/IU/05_Rarity/NFT_Ultra_Rare_UsJL-Encs.jpg"
+              alt="alt"
+            ></img>
           </div>
-        </div>
-        <div class="rarity-pics">
-          <img
-            style={{ padding: "20px" }}
-            src="/assets/1.png"
-            alt="alt"
-          ></img>
-          <img
-            style={{ padding: "20px" }}
-            src="/assets/2.png"
-            alt="alt"
-          ></img>
-        </div>
-      </Stack>
+        </Stack>
+      </div>
     </div>
   );
 }
