043c939222d77abe418687ea37e26473632c04e2
Stephen Dorn <stephendorn@Stephens-MacBook-Pro-2.local>
2022-07-28 21:21:54 -0700

    spotlights in a map function and variable showing, mobile/desktop versions of episodes and spotlights set up

--- src/Components/Episodes.js
+++ src/Components/Episodes.js
@@ -1,6 +1,45 @@
 import React, { Component } from "react";
 import "./Episodes.css";
 
+const spotlightsArr = [
+  {
+    number: 1,
+    desktop_img: "",
+    mobile_img: "",
+    active: true,
+  },
+  {
+    number: 2,
+    desktop_img: "",
+    mobile_img: "",
+    active: false,
+  },
+  {
+    number: 3,
+    desktop_img: "",
+    mobile_img: "",
+    active: false,
+  },
+  {
+    number: 4,
+    desktop_img: "",
+    mobile_img: "",
+    active: false,
+  },
+  {
+    number: 5,
+    desktop_img: "",
+    mobile_img: "",
+    active: false,
+  },
+  {
+    number: 6,
+    desktop_img: "",
+    mobile_img: "",
+    active: false,
+  },
+];
+
 class Episodes extends Component {
   constructor(props) {
     super(props);
@@ -16,11 +55,17 @@ class Episodes extends Component {
     this.resize();
     // pre-load mobile and non-mobile images
     const backgroundImg = new Image();
-    backgroundImg.src = "https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/EpisodesGuide_BG_v03_UlwcvzPCW.jpg";
-    window["https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/Mobile/EpisodesGuide_BG_Mobile_v02_Fqw1nvJ4J.jpg"] = backgroundImg;
+    backgroundImg.src =
+      "https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/EpisodesGuide_BG_v03_UlwcvzPCW.jpg";
+    window[
+      "https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/Mobile/EpisodesGuide_BG_Mobile_v02_Fqw1nvJ4J.jpg"
+    ] = backgroundImg;
     const mobileBackgroundImg = new Image();
-    mobileBackgroundImg.src = "https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/Mobile/EpisodesGuide_BG_Mobile_v02_Fqw1nvJ4J.jpg";
-    window["https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/Mobile/EpisodesGuide_BG_Mobile_v02_Fqw1nvJ4J.jpg"] = mobileBackgroundImg;
+    mobileBackgroundImg.src =
+      "https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/Mobile/EpisodesGuide_BG_Mobile_v02_Fqw1nvJ4J.jpg";
+    window[
+      "https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/Mobile/EpisodesGuide_BG_Mobile_v02_Fqw1nvJ4J.jpg"
+    ] = mobileBackgroundImg;
   }
 
   resize() {
@@ -28,11 +73,29 @@ class Episodes extends Component {
   }
 
   getBackgroundImage(innerWidth) {
-    if (innerWidth <= 600) return "https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/Mobile/EpisodesGuide_BG_Mobile_v02_Fqw1nvJ4J.jpg";
+    if (innerWidth <= 600)
+      return "https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/Mobile/EpisodesGuide_BG_Mobile_v02_Fqw1nvJ4J.jpg";
     return "https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/EpisodesGuide_BG_v03_UlwcvzPCW.jpg";
   }
 
+  getSpotlightImage(innerWidth, index) {
+    if (innerWidth <= 600) return spotlightsArr[index].mobile_img;
+    return spotlightsArr[index].desktop_img;
+  }
+
   render() {
+    const spotlights = spotlightsArr.map((s) => {
+      return (
+        <img
+          key={s.number}
+          src={this.getSpotlightImage(this.state.innerWidth, s.number - 1)}
+          alt={s.number}
+          class="spotlight-img"
+          style={{ display: s.active ? "block" : "none" }}
+        />
+      );
+    });
+
     return (
       <div class="episode-guide">
         <img
@@ -41,11 +104,7 @@ class Episodes extends Component {
           alt="Episode Guide"
         />
         <section id="episode-guide-section" />
-        <img
-          class="spotlight-1-img"
-          src="https://ik.imagekit.io/5ierklngtbt/IU/04_Episode_Guide/Spot_Light_01.png"
-          alt="Spotlight 1"
-        />
+        {spotlights}
         <div>
           <h2 class="episode-guide-title">EPISODE GUIDE</h2>
           <text class="episode-guide-description">
