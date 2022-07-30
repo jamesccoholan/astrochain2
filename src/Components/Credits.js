03f8e28eaa09cc7855c99df6b3e812c454015bd1
Stephen Dorn <stephendorn@stephens-mbp-2.lan>
2022-07-29 19:37:35 -0700

    credits update

--- src/Components/Credits.js
+++ src/Components/Credits.js
@@ -3,29 +3,28 @@ import { Stack } from "@mui/material";
 
 function Credits() {
   return (
-    <Stack
-      direction={{ xs: "column", sm: "column", md: "row", lg: "row" }}
-      spacing={{ xs: 0, sm: 2, md: 4, lg: 10 }}
-      alignItems="center"
-      width="100%"
-      height="auto"
-      object-fit="cover"
-      overflow="auto"
-      style={{ justifyContent: "center", padding: "20px 0 40px 0" }}
-    >
-      <div class="credits-text">
-        A list for all animators and team members in pop out window
-      </div>
-      <div class="credits-text">
-        A list for all animators and team members in pop out window
-      </div>
-      <div class="credits-text">
-        A list for all animators and team members in pop out window
-      </div>
-      <div class="credits-text">
-        A list for all animators and team members in pop out window
-      </div>
-    </Stack>
+    <div class="credits">
+      <Stack
+        direction={{ xs: "column", sm: "column", md: "row", lg: "row" }}
+        spacing={{ xs: 0, sm: 2, md: 4, lg: 10 }}
+        alignItems="center"
+        width="100%"
+        height="auto"
+        object-fit="cover"
+        overflow="auto"
+        style={{ justifyContent: "center", padding: "20px 0 40px 0" }}
+      >
+        <div class="credits-text">
+          A list for all animators and team members in pop out window
+        </div>
+        <div class="credits-text">
+          A list for all animators and team members in pop out window
+        </div>
+        <div class="credits-text">
+          A list for all animators and team members in pop out window
+        </div>
+      </Stack>
+    </div>
   );
 }
 
