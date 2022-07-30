9978f885796bfc98bb7356d8079eb98018277e9d
Stephen Dorn <stephendorn@stephens-mbp-2.lan>
2022-07-29 19:35:33 -0700

    select episode from episode list

--- src/Components/Episodes/EpisodesList.js
+++ src/Components/Episodes/EpisodesList.js
@@ -7,42 +7,44 @@ import { episodes } from "./episodes-content";
 class EpisodesList extends Component {
   constructor(props) {
     super(props);
+    // set the initial state
+    this.state = {
+      episodeId: 0,
+    };
   }
+
+  selectEpisode = (event) => {
+    this.setState({ episodeId: event.currentTarget.getAttribute("value") });
+  };
 
   render() {
     const episodeSection = episodes[this.props.episodeSection];
 
-    const episodeList = episodeSection.episodeList.map(el => {
-      return (
-        <div class={`playlist-number`}>
-          {el.name}
-        </div>
-      );
+    const episodeList = episodeSection.episodeList.map((el) => {
+      return <div key={el.id} class={`playlist-number`} value={el.pos} onClick={this.selectEpisode}>{el.name}</div>;
     });
 
     return (
       <div class="season-1-background">
         <section id="season1-section" />
+        <h2 class="season-1-title">SEASON 1</h2>
         <Stack
           direction={{ xs: "column", sm: "column", md: "row", lg: "row" }}
-          spacing={{ xs: 0, sm: 0, md: 6, lg: 24 }}
+          spacing={{ xs: 4, sm: 4, md: 6, lg: 24 }}
           justifyContent="center"
           alignItems="center"
-          style={{ padding: "70px 0" }}
+          style={{ padding: "80px 0" }}
         >
-          <div>
-            <h2 class="season-1-title">SEASON 1</h2>
-            <div class="playlist-text">
+          <div class="playlist-text">
+            <div>
               <div>
-                <div>
-                  <h2 class="playlist-title">{episodeSection.group}</h2>
-                  {episodeList}
-                </div>
+                <h2 class="playlist-title">{episodeSection.group}</h2>
+                {episodeList}
               </div>
             </div>
           </div>
           <div class="vimeo-player">
-            <Vimeo video="https://player.vimeo.com/video/728917047" autoplay />
+            <Vimeo video={`https://player.vimeo.com/video/${episodeSection.episodeList[this.state.episodeId].vimeoId}`} autoplay />
           </div>
         </Stack>
       </div>
